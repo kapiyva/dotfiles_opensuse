607c5834e90b1d9d211aa8df8c4ec59cd173367e
kapiyva <ri9.py1201@gmail.com>
2024-05-25 15:32:21 +0900

    シンボリックリンクをsystemctl linkに変更

--- install_desktop.sh
+++ install_desktop.sh
@@ -1,3 +1,3 @@
 cargo install xremap --features kde
-sudo ln -s ~/dotfiles/service/xremap.service /etc/systemd/system/
+sudo systemctl link ~/dotfiles/service/xremap.service
 sudo systemctl enable xremap.service
