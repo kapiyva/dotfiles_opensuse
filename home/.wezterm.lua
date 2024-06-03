1275116528fa6ede524259664a4720adacfdfcca
kapiyva <ri9.py1201@gmail.com>
2024-06-04 01:23:33 +0900

    fix wezterm initial size

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -4,5 +4,7 @@ local config = {}
 config.color_scheme = 'Everforest Dark (Gogh)'
 config.font = wezterm.font("UDEV Gothic 35NF")
 config.font_size = 11.0
+config.initial_rows = 35
+config.initial_cols = 140
 
 return config
