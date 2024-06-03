b28a6eb5b138f517895d2da2773cca2697d5a358
kapiyva <ri9.py1201@gmail.com>
2024-06-03 23:24:37 +0900

    change font size

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm';
 local config = {}
 
 config.color_scheme = 'Everforest Dark (Gogh)'
-config.font = wezterm.font "UDEV Gothic NF"
+config.font = wezterm.font("UDEV Gothic 35NF")
+config.font_size = 11.0
 
 return config
