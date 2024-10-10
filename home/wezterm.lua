c7bcb1b3c540ba9c0da14cdf3523c5354573c738
kapiyva <ri9.py1201@gmail.com>
2024-10-10 22:49:20 +0900

    update config

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.color_scheme = 'Everforest Dark (Gogh)'
 config.font = wezterm.font("UDEV Gothic 35NF")
-config.font_size = 11.0
+config.font_size = 12.0
 config.initial_rows = 35
 config.initial_cols = 140
 
