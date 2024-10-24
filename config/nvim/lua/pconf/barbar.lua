7e00ee6496f893caa3ff880e8a278a6480e2346b
kapiyva <ri9py1201@proton.me>
2024-10-25 00:37:17 +0900

    fix barbar config

--- config/nvim/lua/pconf/barbar.lua
+++ config/nvim/lua/pconf/barbar.lua
@@ -2,6 +2,8 @@ local map = vim.keymap.set
 
 -- move tabs
 map('n', 'tn', '<Cmd>BufferNext<CR>')
-map('n', 'tp', '<Cmd>BufferNext<CR>')
+map('n', 'tp', '<Cmd>BufferPrevious<CR>')
 -- close buffer
 map('n', 'tc', '<Cmd>BufferClose<CR>')
+-- tab jump
+map('n', 'tj', '<Cmd>BufferPick<CR>')
