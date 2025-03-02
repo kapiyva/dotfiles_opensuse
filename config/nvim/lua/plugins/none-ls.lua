6430fc812e1ba66dcc9d67571d24333e4aada292
kapiyva <ri9py1201@proton.me>
2025-03-02 11:46:05 +0900

    update none-ls config

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -4,12 +4,11 @@ local diagnostics = require "null-ls.diagnostics"
 null_ls.setup {
   sources = {
     diagnostics.eslint,
-
     null_ls.builtins.formatting.prettier.with {
       prefer_local = "node_modules/.bin",
     },
-
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.completion.spell,
 
     null_ls.builtins.diagnostics.codespell.with({
       diagnostics_postprocess = function(diagnostic)
