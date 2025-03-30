17cc0caa4d559d807fc932bd340703620d4c84ed
kapiyva <ri9py1201@proton.me>
2025-03-30 10:13:49 +0900

    update config

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -2,12 +2,18 @@ local wezterm = require 'wezterm';
 local config = wezterm.config_builder()
 -- theme
 config.color_scheme = 'Everforest Dark (Gogh)'
-config.font = wezterm.font("UDEV Gothic 35NF")
-config.font_size = 12.0
+config.font = wezterm.font("UDEV Gothic NF")
+config.font_size = 11.0
 config.initial_rows = 35
 config.initial_cols = 140
+config.use_ime = true
+config.window_background_opacity = 0.90
 config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
 -- key config
+config.use_ime = true
 config.keys = {
   {
     key = '\\',
