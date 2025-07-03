24be36716a1086cabea396c47776c906f0aac490
kapiyva <ri9py1201@proton.me>
2025-07-03 23:22:29 +0900

    update mason config

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,17 +1,18 @@
 require("mason").setup()
 require("mason-tool-installer").setup({
-	ensure_installed = {
-		"cspell",
-		"rust-analyzer",
-		"typescript-language-server",
-		"eslint-lsp",
-		"prettier",
-		"lua-language-server",
-		"stylua",
-	},
-	auto_update = true,
-	run_on_start = true,
-	integrations = {
-		["mason-lspconfig"] = true,
-	},
+  ensure_installed = {
+    "cspell",
+    "rust-analyzer",
+    "typescript-language-server",
+    "eslint-lsp",
+    "prettier",
+    "lua-language-server",
+    "stylua",
+    "tailwindcss-language-server",
+  },
+  auto_update = true,
+  run_on_start = true,
+  integrations = {
+    ["mason-lspconfig"] = true,
+  },
 })
