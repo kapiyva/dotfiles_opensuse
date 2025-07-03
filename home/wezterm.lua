f219a402e62afce057ce4c60a62f402d42cdb70f
kapiyva <ri9py1201@proton.me>
2025-07-03 23:19:37 +0900

    Revert "remove wezterm"
    This reverts commit d34b3e7e1a2fd7073a84702acd47907d3a54ab90.

---
+++ home/wezterm.lua
@@ -0,0 +1,40 @@
+local wezterm = require 'wezterm';
+local config = wezterm.config_builder()
+-- theme
+config.color_scheme = 'Everforest Dark (Gogh)'
+config.font = wezterm.font("UDEV Gothic NF")
+config.font_size = 11.0
+config.initial_rows = 35
+config.initial_cols = 140
+config.use_ime = true
+config.window_background_opacity = 0.90
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+-- key config
+config.use_ime = true
+config.keys = {
+  {
+    key = '\\',
+    mods = 'ALT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '|',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'ALT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'W',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+
+return config
