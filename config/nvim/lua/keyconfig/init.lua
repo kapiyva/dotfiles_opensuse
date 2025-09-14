45fd3e4ea6b16a4cebbd4da0f89dc1c417711dd3
kapiyva <ri9py1201@proton.me>
2025-09-14 22:56:57 +0900

    keyconfigを切り出し

---
+++ config/nvim/lua/keyconfig/init.lua
@@ -0,0 +1,21 @@
+-- setup commands
+vim.api.nvim_create_user_command("SetupLocalConfig", function()
+	local script = vim.fn.expand("$MYVIMRC/lua/scripts/setup_local_config.lua")
+	vim.cmd("luafile " .. script)
+end, { desc = "Set up project-local .nvim" })
+
+-- key config
+vim.g.mapleader = ","
+vim.keymap.set("n", "<Esc><Esc>", ":nohl<CR>")
+
+-- Terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.api.nvim_create_user_command("T", function()
+	vim.cmd("tabnew |terminal")
+end, {})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	command = "startinsert",
+})
+
