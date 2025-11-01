b8ea90a6287d83c21a03110fac7630febf69f2ec
kapiyva <ri9py1201@proton.me>
2025-11-02 00:47:53 +0900

    organizeImportをするよう修正

--- config/nvim/lua/lsp/typescript.lua
+++ config/nvim/lua/lsp/typescript.lua
@@ -8,6 +8,16 @@ lsp.setup("ts_ls", {
     },
   },
   format = function(args)
+    -- Organize imports before formatting
+    vim.lsp.buf.code_action({
+      context = {
+        only = { "source.organizeImports.ts" },
+        diagnostics = {},
+      },
+      apply = true,
+    })
+    vim.wait(100) -- Wait for organize imports to complete
+    
     conform.format({
       bufnr = args.bufnr,
       timeout_ms = 1000,
