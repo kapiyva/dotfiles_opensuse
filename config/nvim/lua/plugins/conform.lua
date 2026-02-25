edafc39729139b80a468354967a8603d08b111c5
kapiyva <ri9py1201@proton.me>
2026-02-25 10:29:30 +0900

    update conform config

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,13 @@
 require("conform").setup({
   formatters_by_ft = {
-    lua = { "stylua" },
-    rust = { "leptosfmt", "rustfmt" },
-    markdown = { "prettier" },
     javascript = { "prettier" },
     typescript = { "prettier" },
     javascriptreact = { "prettier" },
     typescriptreact = { "prettier" },
+    markdown = { "prettier" },
+    yaml = { "prettier" },
+    lua = { "stylua" },
+    rust = { "leptosfmt", "rustfmt" },
   },
   format_on_save = {
     timeout_ms = 500,
