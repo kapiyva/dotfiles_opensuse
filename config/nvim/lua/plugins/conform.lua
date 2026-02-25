51ffb27d6aa7c2be47fe118efc815bddfca42ef9
kapiyva <ri9py1201@proton.me>
2026-02-25 10:27:58 +0900

    update alacritty config

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,7 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     rust = { "leptosfmt", "rustfmt" },
+    markdown = { "prettier" },
     javascript = { "prettier" },
     typescript = { "prettier" },
     javascriptreact = { "prettier" },
